3eacfcb28f670e883e68c4a69f9a55aabd13c5ac
Dokcvet <vasilii_cvetkov@mail.ru>
2021-10-11 14:51:02 +0300

    Complete Task-6

--- Task-5.py
+++ Task-5.py
@@ -1,7 +1,11 @@
-profit = int(input('Введите Вашу прибыль: '))
-outlay = int(input('Введите Ваш убыток: '))
+profit = float(input('Введите Вашу прибыль: '))
+outlay = float(input('Введите Ваш убыток: '))
 staff = int(input('Введите численнность Вашего персонала: '))
-if profit - outlay > 0:
-    print(f"Ваша фирма работала с прибылью. Рентабельность составила {profit / outlay * 100:02} %."
-          f"На каждого из сотрудников пришлось {profit / staff:02}")
-elif pro
+result = profit - outlay
+if result > 0:
+    print(f"Ваша фирма работала с прибылью. Рентабельность составила {100 * profit / outlay:.1f} %."
+          f" На каждого из сотрудников пришлось {profit / staff:.2f}")
+elif result < 0:
+    print('Ваша фирма работала с убылью.')
+else:
+    print('Ваша фирма работала без прибыли, но и без убытка')
